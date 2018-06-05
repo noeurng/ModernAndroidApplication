2f9ed23948132f18f9b37d7491b687c56d5618f8
Ne1c <nikolay.kucheriaviy@gmail.com>
2018-06-05 14:15:06 +0300

    Added simple style. SplashActivity, MainActivity

---
+++ app/src/main/java/com/ne1c/modernapp/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ne1c.modernapp.presentation.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ne1c.modernapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ne1c/modernapp/presentation/splash/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.ne1c.modernapp.presentation.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.ne1c.modernapp.R
+import com.ne1c.modernapp.presentation.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // I haven't a designer and therefore I was forced use layout instead of drawable
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
+    }
+}
